ee5682e6c41e5016882292a7d58847d9c002d4c9
Matt Dyer <git-madmatt1220@gmail.com>
2014-10-04 09:40:33 -0600

    fixing build issues

---
+++ android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/AndroidManifest.xml : \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/AndroidManifest.xml \
---
+++ android/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/gen/com/secrethq/jumper/R.java \
+ : /home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/res/drawable/spacer.png \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/res/drawable-ldpi/icon.png \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/res/drawable-hdpi/icon.png \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/res/layout/main.xml \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/res/drawable-xhdpi/icon.png \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/res/values/strings.xml \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/res/drawable-mdpi/icon.png \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/AndroidManifest.xml \
