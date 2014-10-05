d24ebf32b55d8a1d694440847100b21df496b270
Matt Dyer <git-madmatt1220@gmail.com>
2014-10-05 11:32:26 -0600

    generated debug apk

---
+++ android/bin/PZPlayer-debug-unaligned.apk.d
@@ -0,0 +1,6 @@
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/PZPlayer-debug-unaligned.apk : \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/PZPlayer.ap_ \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes.dex \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/libs/cocos2d-x/cocos2dx/platform/android/java/bin/classes.jar \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/libs/android-support-v4.jar \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/libs/armeabi/libgame.so \
---
+++ android/bin/classes.dex.d
@@ -0,0 +1,13 @@
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes.dex : \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/R$id.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/R.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/JumperApp.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/JumperActivity$1.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/R$attr.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/BuildConfig.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/JumperActivity.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/R$string.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/R$layout.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/classes/com/secrethq/jumper/R$drawable.class \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/dexedLibs/classes-f6eb0dd119b829b531a9f22f6846f81b.jar \
+/home/matt/cocos2dprojects/JumpGame_SourceCode/Jumper_Android/android/bin/dexedLibs/android-support-v4-68f972ad940e7261c7bb672d1cda6e2d.jar \
